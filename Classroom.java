0f6329ff6f609e79cfb804a48d14852c44c01e5d
thibaultggb <thibault.gagnebien@gmail.com>
2019-05-28 13:32:40 +0200

    finished my quest

---
+++ Classroom.java
@@ -0,0 +1,8 @@
+class Classroom {
+
+    public static void main(String[] args) {
+        Wilder Thibault = new Wilder ("Thibault", true);
+        System.out.println (Thibault.whoAmI());
+    }
+}
+
